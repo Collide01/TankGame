894829e32ffdd941680618784232c13fe0fcc23e
cstrauch01@gmail.com <cstrauch01@gmail.com>
2023-05-10 20:37:34 -0700

    Health script finished for lecture

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,16 +10,31 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set health to max
         currentHealth = maxHealth;
     }
 
-    public void TakeDamage()
+    public void TakeDamage(float amount, Pawn source)
     {
+        currentHealth = currentHealth - amount;
+        Debug.Log(source.name + " did " + amount + " damage to " + gameObject.name);
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
+        if (currentHealth <= 0)
+        {
+            OnDie(source);
+        }
     }
 
-    public void OnDie()
+    public void Heal(float amount, Pawn source)
     {
+        currentHealth = currentHealth + amount;
+        Debug.Log(source.name + " healed " + gameObject.name + " by " + amount);
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
 
+    public void OnDie(Pawn source)
+    {
+        Destroy(gameObject);
     }
 }
