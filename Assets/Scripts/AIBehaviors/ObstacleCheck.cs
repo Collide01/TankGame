5cdf8f2a613a5416dc871dbcf69145eb5efc1c74
cstrauch01@gmail.com <cstrauch01@gmail.com>
2023-07-01 21:27:55 -0700

    Revert "Minor changes"
    This reverts commit 4f34a7940449b072e38eb98cbc215fcbdb090e51.

--- Assets/Scripts/AIBehaviors/ObstacleCheck.cs
+++ Assets/Scripts/AIBehaviors/ObstacleCheck.cs
@@ -4,36 +4,30 @@ using UnityEngine;
 
 public class ObstacleCheck : MonoBehaviour
 {
-    private List<Collider> obstacles;
-    public List<Vector3> closestPoints;
-    public List<Vector3> directions;
-    public List<float> distances;
+    private List<Collider> obstacles = new List<Collider>();
+    public List<Vector3> closestPoints = new List<Vector3>();
+    public List<Vector3> directions = new List<Vector3>();
+    public List<float> distances = new List<float>();
 
     // Start is called before the first frame update
     void Start()
     {
-        obstacles = new List<Collider>();
-        closestPoints = new List<Vector3>();
-        directions = new List<Vector3>();
-        distances = new List<float>();
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // Clears the lists
         closestPoints.Clear();
         directions.Clear();
         distances.Clear();
         
-        // Adds the points on each obstacle closest to the tank
         foreach (Collider obstacle in obstacles)
         {
             closestPoints.Add(obstacle.ClosestPoint(transform.position));
         }
         if (closestPoints.Count > 0)
         {
-            // Gets the direction towards the points and distances from each obstacle
             for (int i = 0; i < closestPoints.Count; i++)
             {
                 directions.Add(closestPoints[i] - transform.position);
