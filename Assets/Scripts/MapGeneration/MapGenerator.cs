97243c1246401dc28abe0c74b2e8c973252e8f6f
cstrauch01@gmail.com <cstrauch01@gmail.com>
2023-07-16 22:08:43 -0700

    Scale works and doors open now

--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -47,6 +47,7 @@ public class MapGenerator : MonoBehaviour
 
                 // Create a new grid at the appropriate location
                 GameObject tempRoomObj = Instantiate(RandomRoomPrefab(), newPosition, Quaternion.identity) as GameObject;
+                tempRoomObj.transform.localScale = new Vector3(roomWidth / 50, tempRoomObj.transform.localScale.y, roomHeight / 50);
 
                 // Set its parent
                 tempRoomObj.transform.parent = this.transform;
@@ -59,6 +60,40 @@ public class MapGenerator : MonoBehaviour
 
                 // Save it to the grid array
                 grid[currentCol, currentRow] = tempRoom;
+
+                // Open the doors
+                // If we are on the bottom row, open the north door
+                if (currentRow == 0)
+                {
+                    tempRoom.doorNorth.SetActive(false);
+                }
+                else if (currentRow == rows - 1)
+                {
+                    // Otherwise, if we are on the top row, open the south door
+                    Destroy(tempRoom.doorSouth);
+                }
+                else
+                {
+                    // Otherwise, we are in the middle, so open both doors
+                    Destroy(tempRoom.doorNorth);
+                    Destroy(tempRoom.doorSouth);
+                }
+                // If we are on the bottom col, open the east door
+                if (currentCol == 0)
+                {
+                    tempRoom.doorEast.SetActive(false);
+                }
+                else if (currentCol == cols - 1)
+                {
+                    // Otherwise, if we are on the top col, open the west door
+                    Destroy(tempRoom.doorWest);
+                }
+                else
+                {
+                    // Otherwise, we are in the middle, so open both doors
+                    Destroy(tempRoom.doorWest);
+                    Destroy(tempRoom.doorEast);
+                }
             }
         }
     }
