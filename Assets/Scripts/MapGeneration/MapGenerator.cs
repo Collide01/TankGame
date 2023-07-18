0b72a12a250ad99196b726aa3228ecefdeaa010e
cstrauch01@gmail.com <cstrauch01@gmail.com>
2023-07-18 13:00:19 -0700

    Added seeding and adjusted rooms

--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -1,47 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject[] gridPrefabs;
-    public int rows;
-    public int cols;
-    public float roomWidth = 50.0f;
-    public float roomHeight = 50.0f;
+    public List<GameObject> roomPrefabs;
+    public int rows = 3;
+    public int columns = 3;
+    public float roomWidth = 50f;
+    public float roomHeight = 50f;
     private Room[,] grid;
+    public int mapSeed = 13;
+    public enum RandomType { Seeded, Random, MapOfTheDay }
+    public RandomType randomType = RandomType.Seeded;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         GenerateMap();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     // Returns a random room
     public GameObject RandomRoomPrefab()
     {
-        return gridPrefabs[Random.Range(0, gridPrefabs.Length)];
+        return roomPrefabs[UnityEngine.Random.Range(0, roomPrefabs.Count)];
+    }
+    public int DateToInt(DateTime dateToUse)
+    {
+        // Add our date up and return it
+        return (dateToUse.Month * 1000000) + (dateToUse.Day * 10000) + dateToUse.Year;
+
+    }
+
+    public int DateAndTimeToInt(DateTime dateToUse)
+    {
+        return DateToInt(dateToUse) + dateToUse.Hour + dateToUse.Minute + dateToUse.Second + dateToUse.Millisecond;
     }
 
     public void GenerateMap()
     {
+        switch (randomType)
+        {
+            case RandomType.Random:
+                mapSeed = DateAndTimeToInt(DateTime.Now);
+                break;
+            case RandomType.Seeded:
+                break;
+            case RandomType.MapOfTheDay:
+                mapSeed = DateToInt(DateTime.Now.Date);
+                break;
+            default:
+                Debug.LogWarning("Unrecognized type of random map");
+                break;
+        }
+        UnityEngine.Random.InitState(mapSeed);
         // Clear out the grid - "column" is our X, "row" is our Y
-        grid = new Room[cols, rows];
+        grid = new Room[columns, rows];
 
         // For each grid row...
         for (int currentRow = 0; currentRow < rows; currentRow++)
         {
             // for each column in that row
-            for (int currentCol = 0; currentCol < cols; currentCol++)
+            for (int currentColumn = 0; currentColumn < columns; currentColumn++)
             {
                 // Figure out the location. 
-                float xPosition = roomWidth * currentCol;
+                float xPosition = roomWidth * currentColumn;
                 float zPosition = roomHeight * currentRow;
                 Vector3 newPosition = new Vector3(xPosition, 0.0f, zPosition);
 
@@ -53,46 +76,30 @@ public class MapGenerator : MonoBehaviour
                 tempRoomObj.transform.parent = this.transform;
 
                 // Give it a meaningful name
-                tempRoomObj.name = "Room_" + currentCol + "," + currentRow;
+                tempRoomObj.name = "Room_" + currentColumn + "," + currentRow;
 
                 // Get the room object
                 Room tempRoom = tempRoomObj.GetComponent<Room>();
 
                 // Save it to the grid array
-                grid[currentCol, currentRow] = tempRoom;
+                grid[currentColumn, currentRow] = tempRoom;
 
-                // Open the doors
-                // If we are on the bottom row, open the north door
+
                 if (currentRow == 0)
                 {
-                    tempRoom.doorNorth.SetActive(false);
+                    grid[currentColumn, currentRow].doorSouth.SetActive(true);
                 }
-                else if (currentRow == rows - 1)
+                if (currentRow == (rows - 1))
                 {
-                    // Otherwise, if we are on the top row, open the south door
-                    Destroy(tempRoom.doorSouth);
+                    grid[currentColumn, currentRow].doorNorth.SetActive(true);
                 }
-                else
+                if (currentColumn == 0)
                 {
-                    // Otherwise, we are in the middle, so open both doors
-                    Destroy(tempRoom.doorNorth);
-                    Destroy(tempRoom.doorSouth);
+                    grid[currentColumn, currentRow].doorWest.SetActive(true);
                 }
-                // If we are on the bottom col, open the east door
-                if (currentCol == 0)
+                if (currentColumn == (columns - 1))
                 {
-                    tempRoom.doorEast.SetActive(false);
-                }
-                else if (currentCol == cols - 1)
-                {
-                    // Otherwise, if we are on the top col, open the west door
-                    Destroy(tempRoom.doorWest);
-                }
-                else
-                {
-                    // Otherwise, we are in the middle, so open both doors
-                    Destroy(tempRoom.doorWest);
-                    Destroy(tempRoom.doorEast);
+                    grid[currentColumn, currentRow].doorEast.SetActive(true);
                 }
             }
         }
