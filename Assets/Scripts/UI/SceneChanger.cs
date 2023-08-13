8c6fdc147e472005fb3da52b14c6a2367df3c838
cstrauch01@gmail.com <cstrauch01@gmail.com>
2023-08-13 00:00:22 -0700

    Implemented scene fade

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -5,8 +5,103 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    private GameObject[] screenFades;
+    private bool fadedIn;
+    private bool beginSwitching;
+    private string sceneName;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        int numberOfFades = 0;
+        // Used to determine if the number of screen fades changed
+        if (screenFades != null)
+        {
+            numberOfFades = screenFades.Length;
+        }
+        screenFades = GameObject.FindGameObjectsWithTag("SceneFade");
+        Debug.Log(screenFades.Length);
+        if (numberOfFades != screenFades.Length)
+        {
+            fadedIn = false;
+        }
+
+        if (!fadedIn)
+        {
+            bool allFaded = false;
+            if (screenFades.Length > 0)
+            {
+                // Fade the scene covers
+                for (int i = 0; i < screenFades.Length; i++)
+                {
+                    Color screenFadeColor = screenFades[i].GetComponent<SpriteRenderer>().color;
+                    screenFadeColor = new Color(screenFadeColor.r, screenFadeColor.g, screenFadeColor.b, screenFadeColor.a - 3f * Time.deltaTime);
+                    screenFades[i].GetComponent<SpriteRenderer>().color = screenFadeColor;
+                    if (screenFadeColor.a <= 0)
+                    {
+                        allFaded = true;
+                    }
+                    else
+                    {
+                        allFaded = false;
+                    }
+                }
+            }
+            else
+            {
+                allFaded = true;
+            }
+
+            // Check if all scene covers have faded
+            if (allFaded)
+            {
+                fadedIn = true;
+            }
+        }
+        else if (beginSwitching)
+        {
+            bool allFaded = false;
+            if (screenFades.Length > 0)
+            {
+                // Fade the scene covers
+                for (int i = 0; i < screenFades.Length; i++)
+                {
+                    Color screenFadeColor = screenFades[i].GetComponent<SpriteRenderer>().color;
+                    screenFadeColor = new Color(screenFadeColor.r, screenFadeColor.g, screenFadeColor.b, screenFadeColor.a + 3f * Time.deltaTime);
+                    screenFades[i].GetComponent<SpriteRenderer>().color = screenFadeColor;
+                    if (screenFadeColor.a >= 1)
+                    {
+                        allFaded = true;
+                    }
+                    else
+                    {
+                        allFaded = false;
+                    }
+                }
+            }
+            else
+            {
+                allFaded = true;
+            }
+
+            // Check if all scene covers have faded
+            if (allFaded)
+            {
+                fadedIn = false;
+                beginSwitching = false;
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+
     public void ChangeScene(string scene)
     {
+        fadedIn = true;
+
         if (scene == "Vehicle")
         {
             if (CreateManager.instance != null)
@@ -47,6 +142,8 @@ public class SceneChanger : MonoBehaviour
                 GameManager.instance.ChangeGameState(GameState.GameOverState);
             }
         }
-        SceneManager.LoadScene(scene);
+
+        sceneName = scene;
+        beginSwitching = true;
     }
 }
